62dddc596154ad63a2ded00aba73cf1da0096d73
slabwu <134193743+slabwu@users.noreply.github.com>
2023-06-13 19:47:20 +0700

    Add text to calculator buttons

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const textDisplay = document.querySelector('.textDisplay');
 const panel = document.querySelector('.panel');
+const BUTTONTEXT = ['C', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', 
+                    '6', '-', '1', '2', '3', '+', '0', '.', 'del', '='];
 
 for (i = 0; i <20; i++) {
     let button = document.createElement('div')
     button.classList.add("button");
-    button.textContent = 'test';
+    button.textContent = BUTTONTEXT[i];
     //div.addEventListener('mouseover', ...);
     panel.appendChild(button);
 }
