04f15ecfa77f316991e50da6753687ed3254be89
slabwu <134193743+slabwu@users.noreply.github.com>
2023-06-13 21:03:20 +0700

    Enable basic operations on calculator

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ for (i = 0; i <20; i++) {
     button.classList.add('button');
     button.classList.add(text);
     if (/\d/.test(text)) button.classList.add('number');
-    if (/[\/x\-\+]/.test(text)) button.classList.add('operator');
+    if (/[%\/x\-\+]/.test(text)) button.classList.add('operator');
+    if (/=/.test(text)) button.classList.add('equal');
     //if (/[%\/x\-\+=]/.test(text)) button.classList.add('operator');
 
     button.textContent = text;
@@ -20,32 +21,42 @@ for (i = 0; i <20; i++) {
 let numbers = [...document.querySelectorAll('.number')];
 let operators = [...document.querySelectorAll('.operator')];
 
-let numberA;
-let operator;
-let numberB;
+let numberA = 9;
+let operator = '+';
+let numberB = 9;
 
 
 
 
-
-
-function addToDisplay() {
-    textDisplay.textContent = `${this.className.slice(7,8)}`;
-}
 
 const arithmetic = {   '+': add = (a,b) => {return +a + +b},
                         '-': subtract = (a,b) => {return +a - +b},
-                        '*': multiply = (a,b) => {return +a * +b},
+                        'x': multiply = (a,b) => {return +a * +b},
                         '/': divide = (a,b) => {return +a / +b},
                         '%': modulus = (a,b) => {return +a % +b},
 }
 
 function operate(a,operator,b) {
-    return arithmetic[operator](a,b);
+    let value = arithmetic[operator](a,b);
+    textDisplay.textContent = value;
+    return value;
+}
+
+function selectNumber() {
+    currentNumber = this.className.slice(7,8);
+    textDisplay.textContent = currentNumber;
+    (!operator) ? numberA = currentNumber: numberB = currentNumber;
+}
+
+function selectOperation() {
+    operator = this.className.slice(7,8)
 }
 
 
 
 
 
-numbers.forEach(number => number.addEventListener('click', addToDisplay))
+numbers.forEach(number => number.addEventListener('click', selectNumber))
+operators.forEach(operator => operator.addEventListener('click', selectOperation))
+document.querySelector('.C').addEventListener('click', () => {textDisplay.textContent = ''})
+document.querySelector('.equal').addEventListener('click', () => {operate(numberA, operator, numberB)})
