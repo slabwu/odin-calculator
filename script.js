491de9185d7dfcddc2c7f4317475ad1da0c6e934
slabwu <134193743+slabwu@users.noreply.github.com>
2023-06-13 20:15:16 +0700

    Enable number input in textDisplay

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const textDisplay = document.querySelector('.textDisplay');
+const textDisplay = document.querySelector('.text-display');
 const panel = document.querySelector('.panel');
 const BUTTONTEXT = ['C', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', 
                     '6', '-', '1', '2', '3', '+', '0', '.', 'del', '='];
@@ -13,6 +13,14 @@ for (i = 0; i <20; i++) {
     if (/[%\/x\-\+=]/.test(text)) button.classList.add('operator');
 
     button.textContent = text;
-    //div.addEventListener('mouseover', ...);
     panel.appendChild(button);
+}
+
+let numbers = [...document.querySelectorAll('.number')];
+let operators = [...document.querySelectorAll('.operator')];
+
+numbers.forEach(number => number.addEventListener('click', addNumber))
+
+function addNumber() {
+    textDisplay.textContent = `${this.className.slice(7,8)}`;
 }
