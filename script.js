fde4de113ac4f90c8c594f070a577e0baf6720b7
slabwu <134193743+slabwu@users.noreply.github.com>
2023-06-14 13:49:11 +0700

    Refactor arithmetic methods

--- script.js
+++ script.js
@@ -23,33 +23,44 @@ let operators = [...document.querySelectorAll('.operator')];
 
 let operator;
 let values = ['', '', ''];
+let finishedCalculation = false;
 
 
 
 
 
-const arithmetic = {   '+': add = (a,b) => {return +a + +b},
-                        '-': subtract = (a,b) => {return +a - +b},
-                        'x': multiply = (a,b) => {return +a * +b},
-                        '/': divide = (a,b) => {return +a / +b},
-                        '%': modulus = (a,b) => {return +a % +b},
+const arithmetic = {   '+': add = (a,b) => +a + +b,
+                        '-': subtract = (a,b) => +a - +b,
+                        'x': multiply = (a,b) => +a * +b,
+                        '/': divide = (a,b) => (!(b == '0'))? `${+a / +b}` : 'LOL',
+                        '%': modulus = (a,b) => +a % +b,
 }
 
 function operate(a,operator,b) {
     let calculatedValue = arithmetic[operator](a,b);
     textDisplay.textContent = calculatedValue;
 
-    values[0] = calculatedValue;
+    values[0] = `${calculatedValue}`;
     values[1] = '';
     values[2] = '';
+    finishedCalculation = true;
 
     return calculatedValue;
 }
 
 function selectNumber() {
-    (!values[1]) ? values[0] += this.className.slice(7,8): values[2] += this.className.slice(7,8);
+     if (!values[1]) {
+        if (finishedCalculation) {
+            values[0] = this.className.slice(7,8);
+            finishedCalculation = false;
+        } else {
+            values[0] += this.className.slice(7,8);
+        }
+     } else {
+        values[2] += this.className.slice(7,8);
+     }
+    
     textDisplay.textContent = values.join(' ');
-
     console.log(values)
 }
 
